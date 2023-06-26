b96d7e9639ea225d030f6cad6635d558c6da9ca2
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-06-26 11:56:44 +0530

    pussing code

---
+++ machine/src/doctor/github.java
@@ -0,0 +1,20 @@
+package doctor;
+
+public class github
+{
+	public void method1()
+	{
+		System.out.println("this is method one ");
+	}
+ public void method2()
+ 
+ {
+	System.out.println("this is method 2"); 
+ }
+ public static void main(String[] args) 
+ {
+	 github obj=new github();
+	 obj.method1();
+	  
+}
+}
