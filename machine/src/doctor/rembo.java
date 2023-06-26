e3cb781f80d3e48d897590855e1b166c9ab1c028
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-06-26 12:03:56 +0530

    pussing 2 nd time

---
+++ machine/src/doctor/rembo.java
@@ -0,0 +1,16 @@
+package doctor;
+
+public class rembo
+
+{
+	public void method2()
+	 
+	 {
+		System.out.println("this is method 2"); 
+	 }
+	 public static void main(String[] args) 
+	 {
+		 github obj=new github();
+		 obj.method1();
+	 }
+}
